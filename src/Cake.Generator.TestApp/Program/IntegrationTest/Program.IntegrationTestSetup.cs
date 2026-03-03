01f61c354aba146648152038552d5d219115ae1e
Mattias Karlsson <matkar@wcom.se>
2026-03-03 23:08:43 +0100

    (GH-92) Use Cake NuGet Feed for cake-build/cake packages
    - Add GitVersion.yml with GitFlow workflow and next-version 6.0.0
    - Add Cake NuGet feed and package source mapping in nuget.config
    - Add integration test nuget.config.template with local, cake, and nuget sources
    - Configure integration tests to add Cake feed and package source mapping
    - Use GitVersion for version and branch in Program.Setup (3-part base version, PreReleaseNumber)
    - Bump Cake.* packages to 6.2.0-alpha0008 in Directory.Packages.props
    - fixes #92

--- src/Cake.Generator.TestApp/Program/IntegrationTest/Program.IntegrationTestSetup.cs
+++ src/Cake.Generator.TestApp/Program/IntegrationTest/Program.IntegrationTestSetup.cs
@@ -5,5 +5,29 @@ public static partial class Program
         data.DotNet("new nugetconfig");
 
         data.DotNet($"nuget add source --name local \"{data.OutputDirectory.FullPath.Replace('/', System.IO.Path.DirectorySeparatorChar)}\"");
+
+        data.DotNet("nuget add source --name cake \"https://pkgs.dev.azure.com/cake-build/Cake/_packaging/cake/nuget/v3/index.json\"");
+
+        var configPath = data.IntegrationTest.NuGetConfig.FullPath;
+        var doc = System.Xml.Linq.XDocument.Load(configPath);
+        var config = doc.Root!;
+        var mapping = new System.Xml.Linq.XElement("packageSourceMapping",
+            new System.Xml.Linq.XElement("packageSource",
+                new System.Xml.Linq.XAttribute("key", "local"),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.Sdk")),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.Generator")),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.Template"))),
+            new System.Xml.Linq.XElement("packageSource",
+                new System.Xml.Linq.XAttribute("key", "cake"),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.Cli")),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.Common")),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.Core")),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.DotNetTool.Module")),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "Cake.NuGet"))),
+            new System.Xml.Linq.XElement("packageSource",
+                new System.Xml.Linq.XAttribute("key", "nuget"),
+                new System.Xml.Linq.XElement("package", new System.Xml.Linq.XAttribute("pattern", "*"))));
+        config.Add(mapping);
+        doc.Save(configPath);
     }
 }
